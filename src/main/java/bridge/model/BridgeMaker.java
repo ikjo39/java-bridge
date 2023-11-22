e0212088d63edabb57de5aeaf818c47474c750a4
ikjo39 <chi4321@naver.com>
2023-11-22 18:08:51 +0900

    fix: 위 아래 다리 생성 로직 오류 수정
    - 다리 생성시 위 아래가 반대로 생성되던 오류 수정

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -5,12 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
 
-    private static final int BRIDGE_UP_NUMBER = 0;
+    private static final int BRIDGE_UP_NUMBER = 1;
     private static final String BRIDGE_UP_SIGN = "U";
     private static final String BRIDGE_DOWN_SIGN = "D";
 
@@ -20,10 +17,6 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
         return IntStream.range(0, size)
                 .map(i -> bridgeNumberGenerator.generate())
